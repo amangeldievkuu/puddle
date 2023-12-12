61f973a1f8edbbf0a770db7663980887fdca45ad
amangeldievkuu <amangeldievkuu@gmail.com>
2023-12-13 00:05:18 +0800

    crud

--- puddle/item/urls.py
+++ puddle/item/urls.py
@@ -5,6 +5,10 @@ from . import views
 app_name = 'item'
 
 urlpatterns = [
-    path('<int:item_id>', views.detail, name='detail'),
+    path('', views.items, name='items'),
+    path('new/', views.new, name='new'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/edit/', views.edit, name='edit'),
+    path('<int:item_id>/', views.detail, name='detail'),
 ]
 
--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from .forms import NewitemForm, EditItemForm
+from item.models import Item, Category
 
-from item.models import Item
+
+def items(request):
+    query = request.GET.get('query', '')
+    categories = Category.objects.all()
+    category_id = request.GET.get('category', 0)
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    return render(request, 'item/items.html', {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id),
+    })
 
 
 # Create your views here.
@@ -9,5 +30,48 @@ def detail(request, item_id):
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=item.id)[:3]
     return render(request, 'item/detail.html', context={
         'item': item,
-        'related_items':related_items,
+        'related_items': related_items,
+    })
+
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewitemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+            return redirect('item:detail', item_id=item.id)
+    else:
+        form = NewitemForm()
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'New Item',
+    })
+
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            item.save()
+            return redirect('item:detail', item_id=item.id)
+    else:
+        form = EditItemForm(instance=item)
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit Item',
     })
